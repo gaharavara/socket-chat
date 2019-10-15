66577a81a7fc255b379df098106d46223d9e0dce
AsociTon <asociton@outlook.com>
2019-10-16 01:33:10 +0530

    Emit events using socket.io.
    Emitting events: emit event 'chat message' and send the chat message entered by the user to be broadcasted on the server via console.log().

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
 });
 
 http.listen(7000, () => {
